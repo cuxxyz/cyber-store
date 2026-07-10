298c9b21e68403aa50a2336e53572d2d6d327350
cuxxyz <cuxxyzz@gmail.com>
2026-07-10 10:59:52 +0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#030303",
+        foreground: "#e2e8f0",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
